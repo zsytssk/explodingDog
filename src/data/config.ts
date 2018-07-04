76a80a1b63426fb865d9f655a131b352cecdcaa8
zsytssk <zsytssk@gmail.com>
2018-07-04 14:03:02 +0800

    [update]test

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,9 +1,9 @@
 export const CONFIG = {
-    env: 'dev',
-    user_id: (window as CusWindow).userId || '',
-    websocket_url: (window as CusWindow).websocketurl || '',
-    publick_key: (window as CusWindow).publicKey || '',
-    token: (window as CusWindow).token || '',
     cdn_url: (window as CusWindow).GAME_CDN_URL || '',
     cdn_version: (window as CusWindow).CDN_VERSION || '',
+    env: 'dev',
+    publick_key: (window as CusWindow).publicKey || '',
+    token: (window as CusWindow).token || '',
+    user_id: (window as CusWindow).userId || '',
+    websocket_url: (window as CusWindow).websocketurl || '',
 };
