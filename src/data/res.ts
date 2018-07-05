103d25839ac4ca090b790b39c6981139d51555dc
章旸 <ex-zhangyang623@pingan.com.cn>
2018-07-05 15:40:48 +0800

    res

--- src/data/res.ts
+++ src/data/res.ts
@@ -11,10 +11,13 @@ export const RES = {
         { url: 'res/atlas/images/hall/rank.atlas', type: Laya.Loader.ATLAS },
         { url: 'res/atlas/images/component.atlas', type: Laya.Loader.ATLAS },
         { url: 'res/atlas/images/cards.atlas', type: Laya.Loader.ATLAS },
+        { url: 'images/hall/btn_bg.png', type: Laya.Loader.IMAGE },
+        { url: 'images/hall/icon_card1.png', type: Laya.Loader.IMAGE },
+        { url: 'images/hall/icon_card2.png', type: Laya.Loader.IMAGE },
+        { url: 'images/hall/icon_card3.png', type: Laya.Loader.IMAGE },
+        { url: 'images/hall/user_panel.png', type: Laya.Loader.IMAGE },
         { url: 'font/hall_user_info.fnt', type: Laya.Loader.FONT },
         { url: 'font/hall_user_info.png', type: Laya.Loader.IMAGE },
-        { url: 'font/join_room.fnt', type: Laya.Loader.FONT },
-        { url: 'font/join_room.png', type: Laya.Loader.IMAGE },
     ],
     GAME: [
         { url: 'res/atlas/images/game.atlas', type: Laya.Loader.ATLAS },
@@ -22,11 +25,15 @@ export const RES = {
     ],
     LOADING: [
         { url: 'res/atlas/images/loading.atlas', type: Laya.Loader.ATLAS },
+        { url: 'res/atlas/images/loading.png', type: Laya.Loader.IMAGE },
         { url: 'animation/loading.png', type: Laya.Loader.IMAGE },
         { url: 'animation/loading.sk', type: Laya.Loader.BUFFER },
     ],
     POP: [
         { url: 'res/atlas/images/hall/pop.atlas', type: Laya.Loader.ATLAS },
         { url: 'res/atlas/images/hall/pop.png', type: Laya.Loader.IMAGE },
+        { url: 'images/hall/pop/bg_join.png', type: Laya.Loader.IMAGE },
+        { url: 'font/join_room.fnt', type: Laya.Loader.FONT },
+        { url: 'font/join_room.png', type: Laya.Loader.IMAGE },
     ],
 };
