779cee567f9c0f2b6445d59294a3cab3c42e09ec
章旸 <ex-zhangyang623@pingan.com.cn>
2018-07-06 11:43:18 +0800

    res

--- src/data/res.ts
+++ src/data/res.ts
@@ -3,6 +3,11 @@ export const RES = {
     COMPONENT: [
         { url: 'res/atlas/images/component.atlas', type: Laya.Loader.ATLAS },
         { url: 'res/atlas/images/component.png', type: Laya.Loader.IMAGE },
+        { url: 'res/atlas/images/component/cards.atlas', type: Laya.Loader.ATLAS },
+        { url: 'res/atlas/images/component/cards.png', type: Laya.Loader.IMAGE },
+        { url: 'images/component/cards/icon_card1.png', type: Laya.Loader.IMAGE },
+        { url: 'images/component/cards/icon_card2.png', type: Laya.Loader.IMAGE },
+        { url: 'images/component/cards/icon_card3.png', type: Laya.Loader.IMAGE },
     ],
     HALL: [
         { url: 'images/bg/bg.jpg', type: Laya.Loader.IMAGE },
@@ -12,9 +17,6 @@ export const RES = {
         { url: 'res/atlas/images/component.atlas', type: Laya.Loader.ATLAS },
         { url: 'res/atlas/images/cards.atlas', type: Laya.Loader.ATLAS },
         { url: 'images/hall/btn_bg.png', type: Laya.Loader.IMAGE },
-        { url: 'images/hall/icon_card1.png', type: Laya.Loader.IMAGE },
-        { url: 'images/hall/icon_card2.png', type: Laya.Loader.IMAGE },
-        { url: 'images/hall/icon_card3.png', type: Laya.Loader.IMAGE },
         { url: 'images/hall/user_panel.png', type: Laya.Loader.IMAGE },
         { url: 'font/hall_user_info.fnt', type: Laya.Loader.FONT },
         { url: 'font/hall_user_info.png', type: Laya.Loader.IMAGE },
