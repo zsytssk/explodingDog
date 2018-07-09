47a9fd2d0273be91466e9dae5d43f0f48bf48012
章旸 <ex-zhangyang623@pingan.com.cn>
2018-07-09 14:24:41 +0800

    hall

--- src/scene/hall/content.ts
+++ src/scene/hall/content.ts
@@ -44,7 +44,7 @@ export class HallContent extends ui.hall.hallcontentUI {
         this.userName.changeText(data.nickname);
         this.level.changeText(`Lv:${data.level}`);
         this.totalRound.changeText(`场次:${data.totalPlayCount}`);
-        this.winrate.changeText(`胜率:${data.winRate * 100}%`);
+        this.winrate.changeText(`胜率:${(data.winRate * 100).toFixed(2)}%`);
         this.score.changeText(`积分:${data.score}`);
         this.expBar.value = data.currentExp / data.nextLevelExp;
     }
