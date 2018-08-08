ae51a0b23206db276a6252864502654fa6e08c50
章旸 <ex-zhangyang623@pingan.com.cn>
2018-08-08 14:49:03 +0800

    userexploding

--- src/scene/popup/popupUserExploded.ts
+++ src/scene/popup/popupUserExploded.ts
@@ -1,13 +1,16 @@
 import { getAvatar } from "../../utils/tool";
 import { Avatar } from "./component/avatar";
+import { getElementsByName } from "../../mcTree/utils/zutil";
+import { scale_in } from "../../mcTree/utils/animate";
 
 export class PopupUserExploded extends ui.popup.popupUserExplodedUI {
     CONFIG = {
         shadowAlpha: 0.8,
         closeByGroup: true,
-        autoClose: 2500
+        autoClose: 5000
     }
     group = 'exploding';
+    private explodeUserAvatar;
     constructor() {
         super();
     }
@@ -20,19 +23,31 @@ export class PopupUserExploded extends ui.popup.popupUserExplodedUI {
         this.remainUser.changeText(data.remainUser);
         this.bombProb.changeText(data.bombProb + '%')
         //显示头像
-        let explodeUserAvatar = null;
         if (Array.isArray(data.userList)) {
             data.userList.forEach((user, index) => {
                 let avatar = new Avatar(user);
                 if (data.explodeUserId == user.userId) {
-                    explodeUserAvatar = avatar;
+                    this.explodeUserAvatar = avatar;
                 }
                 avatar.left = (this.avatarBox.width - 80) / (data.userList.length + 1) * (index + 1);
                 this.avatarBox.addChild(avatar);
             });
         }
-        //动效
 
+    }
+
+    onOpened() {
+        getElementsByName(this, 'infoBox').forEach((item, index) => {
+            this.timerOnce(800 * (index + 1), this, () => {
+                scale_in(item, 300, 'backOut');
+            })
+        });
+        let ani = new Laya.Skeleton();
+        ani.load('animation/user_exploded.sk', new Laya.Handler(this, () => {
+            this.explodeUserAvatar.addChild(ani);
+            ani.pos(0.5 * this.explodeUserAvatar.width, 0.5 * this.explodeUserAvatar.height);
+            ani.play(0, false);
+        }));
     }
 }
 
